0d7109447d740c1cae4465bd3a6a67f4bcfe4916
Rafael Barros <rafaelbarros.df@gmail.com>
2019-07-28 19:47:07 -0300

    :construction: Criando component alterar-contato

---
+++ src/app/components/opcoes/alterar-contato/alterar-contato.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alterar-contato',
+  templateUrl: './alterar-contato.component.html',
+  styleUrls: ['./alterar-contato.component.css']
+})
+export class AlterarContatoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
