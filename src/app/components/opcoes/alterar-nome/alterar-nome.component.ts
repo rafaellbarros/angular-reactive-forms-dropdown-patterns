b2e57e0c40b898c8afbd4abf9b2fcb40555519ac
Rafael Barros <rafaelbarros.df@gmail.com>
2019-07-28 19:46:49 -0300

    :construction: Criando component alterar-nome

---
+++ src/app/components/opcoes/alterar-nome/alterar-nome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alterar-nome',
+  templateUrl: './alterar-nome.component.html',
+  styleUrls: ['./alterar-nome.component.css']
+})
+export class AlterarNomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
