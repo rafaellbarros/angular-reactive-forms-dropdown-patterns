6eda278c2d6347be8cdf563e4d91b215fcca7959
Rafael Barros <rafaelbarros.df@gmail.com>
2019-07-29 00:31:47 -0300

    :construction: Implementando alterarContato

--- src/app/components/opcoes/alterar-contato/alterar-contato.component.ts
+++ src/app/components/opcoes/alterar-contato/alterar-contato.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'alterar-contato',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alterar-contato.component.css']
 })
 export class AlterarContatoComponent implements OnInit {
+
+  @Input()
+  alterarContatoForm: FormGroup;
 
   constructor() { }
 
