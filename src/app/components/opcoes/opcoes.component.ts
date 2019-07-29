72966e50cdb951613043181ffc57f617255c1098
Rafael Barros <rafaelbarros.df@gmail.com>
2019-07-28 21:57:05 -0300

    :construction: Implementando opcaoSelecionada

--- src/app/components/opcoes/opcoes.component.ts
+++ src/app/components/opcoes/opcoes.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { OpcoesService } from './opcoes.service';
 import { Opcao } from '../models/opcoes.model';
+import { Constants } from 'src/app/shared/constants';
+
 
 @Component({
   selector: 'opcoes',
@@ -10,9 +12,11 @@ import { Opcao } from '../models/opcoes.model';
 })
 export class OpcoesComponent implements OnInit {
 
+  public readonly ID_ALTERAR_NOME = Constants.ID_ALTERAR_NOME;
+  public readonly ID_ALTERAR_CONTATO = Constants.ID_ALTERAR_CONTATO;
+
   opcoesForm: FormGroup;
   formInitialize = false;
-
 
   opcoes: Opcao[];
 
@@ -21,7 +25,6 @@ export class OpcoesComponent implements OnInit {
   ngOnInit() {
     this.initForm();
     this.opcoes = this.opcoesService.opcoes;
-    console.log(this.opcoes);
   }
 
 
@@ -30,6 +33,18 @@ export class OpcoesComponent implements OnInit {
       opcao: ['']
     });
     this.formInitialize = true;
+  }
+
+  onChangeOpcoes(event) {
+    console.warn(event);
+  }
+
+  get opcaoSelecionada() {
+    return  this.getOpcoesForm().get('opcao').value != null ? this.getOpcoesForm().get('opcao').value.id : null;
+  }
+
+  getOpcoesForm(): FormGroup {
+    return this.opcoesForm;
   }
 
 }
