ba99162a1d0c61f0357c8226d6094852763fce9d
Rafael Barros <rafaelbarros.df@gmail.com>
2019-07-29 00:31:22 -0300

    :construction: Implementando alterarNome

--- src/app/components/opcoes/alterar-nome/alterar-nome.component.ts
+++ src/app/components/opcoes/alterar-nome/alterar-nome.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'alterar-nome',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlterarNomeComponent implements OnInit {
 
+  @Input()
+  alterarNomeForm: FormGroup;
+
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
